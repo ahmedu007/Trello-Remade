5d538f1810839f7124b98e0a459e1d8200c6df63
Umair Ahmed <umair170@hotmail.com>
2018-01-06 13:49:03 +0000

    Removing comments

--- src/components/BeautifulDND.js
+++ src/components/BeautifulDND.js
@@ -3,15 +3,6 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { ListItem, ListItemIcon, ListItemText } from "material-ui/List";
 import DeleteIcon from "material-ui-icons/Delete";
 
-// fake data generator
-// const getItems = count =>
-//   Array.from({ length: count }, (v, k) => k).map(k => ({
-//     task_id: `item-${k}`,
-//     text: `item ${k}`,
-//     list_id: `itemList ${k}`
-//   }));
-
-// a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
@@ -20,18 +11,14 @@ const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
-// using some little inline style helpers to make the app look okay
 const grid = 8;
 const getItemStyle = (draggableStyle, isDragging) => ({
-  // some basic styles to make the items look a bit nicer
   userSelect: "none",
   padding: grid * 2,
   margin: `0 0 ${grid}px 0`,
 
-  // change background colour if dragging
   background: isDragging ? "lightgreen" : "grey",
 
-  // styles we need to apply on draggables
   ...draggableStyle
 });
 const getListStyle = isDraggingOver => ({
@@ -64,7 +51,6 @@ class BeautifulDND extends Component {
   }
 
   onDragEnd(result) {
-    // dropped outside the list
     if (!result.destination) {
       return;
     }
@@ -84,8 +70,6 @@ class BeautifulDND extends Component {
     this.props.removeTask(this.props.task.task_id);
   };
 
-  // Normally you would want to split things out into separate components.
-  // But in this example everything is just done in one place for simplicity
   render() {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
@@ -96,7 +80,6 @@ class BeautifulDND extends Component {
               style={getListStyle(snapshot.isDraggingOver)}
             >
               {this.state.items.map(item => (
-                // {this.props.task.map(item => (
                 <Draggable key={item.task_id} draggableId={item.task_id}>
                   {(provided, snapshot) => (
                     <div>
@@ -124,7 +107,6 @@ class BeautifulDND extends Component {
                             />
                           </ListItemIcon>
                         </ListItem>
-                        {/* {item.text} */}
                       </div>
                       {provided.placeholder}
                     </div>
