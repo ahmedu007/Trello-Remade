0fe7474e831f00713f27027eb0ec193bdc3f0f83
Umair Ahmed <umair170@hotmail.com>
2018-01-10 19:25:30 +0000

    Flex inputfield

--- src/components/AddList.js
+++ src/components/AddList.js
@@ -1,7 +1,34 @@
 import React from "react";
 import shortid from "shortid";
-
+import Icon from "material-ui/Icon";
 import Paper from "material-ui/Paper";
+import { withStyles } from "material-ui/styles";
+import TextField from "material-ui/TextField";
+import Button from "material-ui/Button";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  menu: {
+    width: 200
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit
+  }
+});
 
 class AddList extends React.Component {
   constructor(props) {
@@ -32,22 +59,35 @@ class AddList extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
+
     return (
       <Paper style={{ maxWidth: "25%", marginTop: "0.8%", height: "10%" }}>
         <div className="column">
-          <form onSubmit={this.handleSubmit}>
-            <div className="group" style={{ marginTop: "4%" }}>
-              <input
-                type="text"
-                required
-                onChange={this.handleChange}
-                value={this.state.title}
-              />
-              <span className="highlight" />
-              <span className="bar" />
-              <label>Add a List</label>
-            </div>
-            <input type="submit" value="Add" />
+          <form
+            onSubmit={this.handleSubmit}
+            className={classes.container}
+            autoComplete="off"
+          >
+            <TextField
+              id="with-placeholder"
+              required
+              label="Add a new List"
+              placeholder="Title"
+              className={classes.textField}
+              margin="normal"
+              onChange={this.handleChange}
+              value={this.state.title}
+            />
+            <Button
+              className={classes.button}
+              raised
+              color="primary"
+              type="submit"
+            >
+              Send
+              <Icon className={classes.rightIcon}>send</Icon>
+            </Button>
           </form>
         </div>
       </Paper>
@@ -55,4 +95,4 @@ class AddList extends React.Component {
   }
 }
 
-export default AddList;
+export default withStyles(styles)(AddList);
