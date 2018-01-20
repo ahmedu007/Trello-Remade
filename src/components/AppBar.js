2df75827eeb7175879cd4788243a0679d979934f
ahmedu007 <umair170@hotmail.com>
2018-01-20 14:38:43 +0000

    re-centering navbar text

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,15 +1,15 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "material-ui/styles";
-import AppBar from "material-ui/AppBar";
-import Toolbar from "material-ui/Toolbar";
-import Typography from "material-ui/Typography";
-import Button from "material-ui/Button";
-import DrawersMenu from "./DrawersMenu";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
+import DrawersMenu from './DrawersMenu'
 
 const styles = {
   root: {
-    width: "100%"
+    width: '100%'
   },
   flex: {
     flex: 1
@@ -18,25 +18,25 @@ const styles = {
     marginLeft: -12,
     marginRight: 20
   }
-};
+}
 
-function ButtonAppBar(props) {
-  const { classes } = props;
+function ButtonAppBar (props) {
+  const { classes } = props
   return (
-    <AppBar>
+    <AppBar style={{ textAlign: 'center' }}>
       <Toolbar>
         <DrawersMenu />
-        <Typography type="title" color="inherit" className={classes.flex}>
+        <Typography type='title' color='inherit' className={classes.flex}>
           Welcome to Custom Trello Board
         </Typography>
-        <Button color="contrast">Login</Button>
+        <Button color='contrast'>Login</Button>
       </Toolbar>
     </AppBar>
-  );
+  )
 }
 
 ButtonAppBar.propTypes = {
   classes: PropTypes.object.isRequired
-};
+}
 
-export default withStyles(styles)(ButtonAppBar);
+export default withStyles(styles)(ButtonAppBar)
