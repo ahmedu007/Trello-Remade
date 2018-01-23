9044e85d805bb20848eda2ee32e306f7ffaf4c95
ahmedu007 <umair170@hotmail.com>
2018-01-23 16:24:57 +0000

    Adding new title for boards

--- src/Boards.js
+++ src/Boards.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
+import shortid from "shortid";
 
 class Boards extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      boards: [
+        {
+          board_id: shortid.generate(),
+          title: "Welcome Board"
+        }
+      ],
+      newTitle: ""
+    };
   }
+
+  addNewBoard(title) {
+    let boards = this.state.boards.concat({
+      board_id: shortid.generate(),
+      title
+    });
+    this.setState({
+      boards
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.addNewBoard(this.state.newTitle);
+    this.setState({
+      newTitle: ""
+    });
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({ newTitle: event.target.value });
+  }
+
   render() {
     return (
       <div className="container">
@@ -13,12 +46,21 @@ class Boards extends Component {
         <br />
         <br />
         <div className="box">
-          <div className="columns">
-            {[1, 2, 3].map((box, i) => (
+          <div className="columns" style={{ textAlign: "center" }}>
+            {this.state.boards.map((board, i) => (
               <div className="column is-one-quarter" key={i}>
-                <div className="box">Column {i + 1}</div>
+                <div className="box">{board.title}</div>
               </div>
             ))}
+            <div className="column is-one-quarter">
+              <form onSubmit={this.handleSubmit.bind(this)}>
+                <input
+                  type="text"
+                  onChange={this.handleChange.bind(this)}
+                  value={this.state.newTitle}
+                />
+              </form>
+            </div>
           </div>
         </div>
       </div>
